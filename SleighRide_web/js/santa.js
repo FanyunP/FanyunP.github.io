ec6015a4f363d79fcd2911597fff0f882bce9a94
FanyunP <fanyun@astandardtransmission.com>
2021-08-12 17:18:22 -0400

    add game mechanics

--- SleighRide_web/js/santa.js
+++ SleighRide_web/js/santa.js
@@ -100,30 +100,36 @@ const bridge = {
 
 //stars
 const starsimg = new Image();
+const starburst = new Image();
 starsimg.src = './img/STAR.png';
+starburst.src = './img/STAR_BURST_.png'
+
 const cloudimg = new Image();
 cloudimg.src = './img/CLOUD_STILL.png';
 const stars = {
 	position_star : [],
 	size_star : [],
-	maxH_star : 120,
+	maxH_star : 80,
 	speed_star : 0.3,
 
 	position_cloud : [],
 	origin_size : {
-		x : 38,
-		y : 21
+		x : 18.5,
+		y : 4.2
 	},
 	size_cloud : [],
-	maxH_cloud :80,
-	speed_cloud : 0.45,
+	maxH_cloud :150,
+	speed_cloud : 0.65,
 	draw : function(){
 		// console.log(this.position.length);
 		for(let i = 0; i < this.position_star.length; i++){
-			ctx.drawImage(starsimg,this.position_star[i].x,this.position_star[i].y,this.size_star[i],this.size_star[i]);
+			ctx.drawImage(starsimg,350,350,400,400,this.position_star[i].x,this.position_star[i].y,this.size_star[i],this.size_star[i]);
 		}
 		for(let i = 0; i < this.position_cloud.length; i++){
-			ctx.drawImage(cloudimg,this.position_cloud[i].x,this.position_cloud[i].y,this.size_cloud[i].w,this.size_cloud[i].h);
+			ctx.drawImage(cloudimg,1000,900,1850,428,this.position_cloud[i].x,this.position_cloud[i].y,this.size_cloud[i].w,this.size_cloud[i].h);
+			// ctx.beginPath();
+			// ctx.rect(this.position_cloud[i].x,this.position_cloud[i].y,this.size_cloud[i].w,this.size_cloud[i].h);
+			// ctx.stroke();
 		}
 	},
 
@@ -131,15 +137,15 @@ const stars = {
 		if(frames%300 == 0){
 			this.position_star.push({
 				x : getRandomInt(260,cvs.width),
-				y : getRandomInt(0,this.maxH_star)
+				y : getRandomInt(20,this.maxH_star)
 			});
-			this.size_star.push(getRandomInt(30,75));
+			this.size_star.push(getRandomInt(15,28));
 		}
-		if(frames%300 == 0){
-			let curRam = getRandomInt(3,7);
+		if(frames%700 == 0){
+			let curRam = getRandomInt(3,8);
 			this.position_cloud.push({
 				x : getRandomInt(200,cvs.width),
-				y : getRandomInt(20,this.maxH_cloud)
+				y : getRandomInt(60,this.maxH_cloud)
 			});
 			this.size_cloud.push({
 				w : this.origin_size.x * curRam,
@@ -149,14 +155,15 @@ const stars = {
 
 		let curThreshold = {
 			xmin : santaSleigh.x,
-			xmax : santaSleigh.x + (santaSleigh.origin_size.w * santaSleigh.size_ratio) * 0.6,
-			ymin : santaSleigh.y - (santaSleigh.origin_size.h * santaSleigh.size_ratio)/2,
-			ymax : santaSleigh.y + (santaSleigh.origin_size.h * santaSleigh.size_ratio)/2
+			xmax : santaSleigh.x + (santaSleigh.origin_size.w * santaSleigh.size_ratio) * 0.9,
+			ymin : santaSleigh.y + 10,
+			ymax : santaSleigh.y + (santaSleigh.origin_size.h * santaSleigh.size_ratio) * 0.8//+ (santaSleigh.origin_size.h * santaSleigh.size_ratio)/2
 		}
+		// console.log(curThreshold.ymin);
 		for(let i = 0; i < this.position_star.length; i++){
 			let p = this.position_star[i];
 			p.x -= this.speed_star;
-			if(p.x + this.size_star[i] <= -20){
+			if(p.x + this.size_star[i] <= -5){
 				this.position_star.shift();
 				this.size_star.shift();
 			}
@@ -164,35 +171,43 @@ const stars = {
 				w : this.size_star[i],
 				h : this.size_star[i]
 			};
-			const isCollidStar = collisionDetection(curThreshold,p,curSize);
-			if(isCollidStar){
-				score.score_state[starCounter] = 1;
-				this.position_star.shift();
-				this.size_star.shift();
-				starCounter += 1;
+			if(this.size_star.length > 0){
+				const isCollidStar = collisionDetection(curThreshold,p,curSize);
+				if(isCollidStar){
+					if(starCounter < 5){
+						score.score_state[starCounter] = 1;
+						starCounter += 1;
+					}
+					this.position_star.shift();
+					this.size_star.shift();
+				}
 			}
 		}
 		for(let i = 0; i < this.position_cloud.length; i++){
 			let p = this.position_cloud[i];
 			p.x -= this.speed_cloud;
-			if(p.x + this.size_cloud[i].w <= -20){
+			if(p.x + this.size_cloud[i].w <= -5){
 				this.position_cloud.shift();
 				this.size_cloud.shift();
 			}
 			// console.log(p.y-this.size_cloud.y - curThreshold.ymin);
-			const isCollidCloud = collisionDetection(curThreshold,p,this.size_cloud[i]);
-			if(isCollidCloud){
-				starCounter -= 1;
-				score.score_state[starCounter] = 0;
-				this.position_cloud.shift();
-				this.size_cloud.shift();
+			if(this.size_cloud.length>0){
+				const isCollidCloud = collisionDetection(curThreshold,p,this.size_cloud[i]);
+				if(isCollidCloud){
+					if(starCounter > 0){
+						starCounter -= 1;
+						score.score_state[starCounter] = 0;
+					}
+					this.position_cloud.shift();
+					this.size_cloud.shift();
+				}
 			}
 		}
-		if(starCounter >= 4){
-			starCounter = 4;
-		}else if(starCounter<=0){
-			starCounter = 0;
-		}
+		// if(starCounter >= 4){
+		// 	starCounter = 4;
+		// }else if(starCounter<=0){
+		// 	starCounter = 0;
+		// }
 		// console.log(starCounter);
 	}
 }
@@ -200,11 +215,11 @@ const stars = {
 function collisionDetection(threshold,input,size){
 	  const pos = [({x : input.x, y : input.y}),({x: input.x + size.w, y: input.y}),
 	  							({x : input.x,y : input.y + size.h}),({x : input.x + size.w, y: input.y + size.h})];
-	  // const pos = [({x : input.x - size.w/2, y : input.y - size.h/2}),({x: input.x + size.w/2, y: input.y - size.h/2}),
-	  // 							({x : input.x - size.w/2,y : input.y + size.h/2}),({x : input.x + size.w/2, y: input.y + size.h/2})];
+	  // const pos = [({x : input.x, y : input.y}),({x: input.x + size.w/2, y: input.y}),
+	  // 							({x : input.x,y : input.y + size.h/2}),({x : input.x + size.w/2, y: input.y + size.h/2})];
 	  for(let i = 0 ; i < pos.length; i++){
 	  	if(pos[i].x > threshold.xmin && pos[i].x < threshold.xmax&& 
-	  		 pos[i].y > threshold.ymin && pos[i].y > threshold.ymax){
+	  		 pos[i].y > threshold.ymin && pos[i].y < threshold.ymax){
 	  		return true;
 	  	}
 	  }
@@ -237,33 +252,38 @@ const forgroundcity = {
 }
 
 //LOAD SANTA
-const santa = new Image();
-const dir = './img/SANTA_SLEIGH/SANTA_SLEIGH_';
-var fileextension = ".png";
-var path;
+const santa = [];
+for(let i = 0; i < 90; i++){
+	let curframe = new Image();
+	curframe.src = './img/SANTA_SLEIGH/SANTA_SLEIGH_' + (i+1) + '.png';
+	santa.push(curframe);
+}
+// const dir = './img/SANTA_SLEIGH/SANTA_SLEIGH_';
+// var fileextension = ".png";
+// var path;
  //"./img/Santa-Ride.js"
 
 const santaSleigh = {
-	x : -20,
+	x : 100,
 	y : 50,
 	origin_size : {
-		w : 432,
-		h : 288
+		w : 200,
+		h : 100
 	},
 	size_ratio : 0.8,
 	frame : 0,
 	frameSpeed : 5,
-	animationFrames : 91,
+	animationFrames : 90,
 
 	gravity : 0.05,
 	jump : 2.5,
 	speed : 0,
-	maxH:100,
+	maxH:150,
 
 	draw : function(){
-		path = dir + this.frame + fileextension;
-		santa.src = path;
-		ctx.drawImage(santa,this.x,this.y,this.origin_size.w * this.size_ratio,this.origin_size.h * this.size_ratio);
+		// path = dir + (this.frame + 1) + fileextension;
+		// santa.src = path;
+		ctx.drawImage(santa[this.frame],568,358,800,315,this.x,this.y,this.origin_size.w * this.size_ratio,this.origin_size.h * this.size_ratio);
 	},
 
 	flap : function(){
